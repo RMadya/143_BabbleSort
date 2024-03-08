6c0abf0c64e91b50ac48d907d2c17f532407bebb
RMadya <kereyoben75@gmail.com>
2024-03-08 11:05:26 +0700

    input2

--- 143_BabbleSort/143_BabbleSort.cpp
+++ 143_BabbleSort/143_BabbleSort.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya element pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 element.\n";
+		}
+	}
+	
+
+}
